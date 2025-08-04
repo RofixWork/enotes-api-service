bac3d930fe6953478e8e8effd837ddc89e7afb2b
Rofix <wourkout123@gmail.com>
2025-08-04 01:41:49 +0100

    initial commit

---
+++ src/test/java/com/rofix/enotes_service/EnotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofix.enotes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
