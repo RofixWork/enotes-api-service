77867ac4c044ce6e8ed3efaeb53ad1704d740e4f
Abdessamad Ait Oughenja <wourkout123@gmail.com>
2025-08-05 18:04:44 +0100

    feat: define logs inside CategoryServiceImpl

--- src/main/java/com/rofix/enotes_service/service/CategoryServiceImpl.java
+++ src/main/java/com/rofix/enotes_service/service/CategoryServiceImpl.java
@@ -8,11 +8,13 @@ import com.rofix.enotes_service.exception.base.NotFoundException;
 import com.rofix.enotes_service.helper.CategoryHelper;
 import com.rofix.enotes_service.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
@@ -28,6 +30,7 @@ public class CategoryServiceImpl implements CategoryService {
         Category newCategory = modelMapper.map(categoryDTO, Category.class);
         newCategory.setCreatedBy(1);
         Category savedCategory = categoryRepository.save(newCategory);
+        log.info("[CategoryServiceImpl] :: [saveCategory] :: Category has been Saved Successfully...");
 
         return modelMapper.map(savedCategory,CategoryResponseDTO.class);
     }
@@ -35,17 +38,20 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<CategoryResponseDTO> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
+        log.info("[CategoryServiceImpl] :: [getAllCategories] :: Get All Categories...");
         return categoryHelper.getCategoryResponseDTO(categories);
     }
 
     @Override
     public List<CategoryResponseDTO> getActiveCategories() {
         List<Category> activeCategories =  categoryRepository.findByIsActiveTrueAndIsDeletedFalse();
+        log.info("[CategoryServiceImpl] :: [getActiveCategories] :: Get All Active Categories...");
         return categoryHelper.getCategoryResponseDTO(activeCategories);
     }
 
     @Override
     public CategoryResponseDTO getCategoryById(Long id) {
+        log.info("[CategoryServiceImpl] :: [getCategoryById] :: Get Category with Id: [{}]", id);
         return modelMapper.map(categoryHelper.getByIdAndActiveAndNotDeletedOrThrow(id), CategoryResponseDTO.class);
     }
 
@@ -56,6 +62,7 @@ public class CategoryServiceImpl implements CategoryService {
         category.setIsDeleted(true);
         categoryRepository.save(category);
 
+        log.info("[CategoryServiceImpl] :: [getCategoryById] :: Category with name [{}] has been deleted successfully...", id);
         return "Category with name '" + category.getName() + "' has been deleted successfully...";
     }
 
@@ -68,6 +75,7 @@ public class CategoryServiceImpl implements CategoryService {
         category = categoryHelper.getUpdatedCategory(category, categoryDTO);
         category = categoryRepository.save(category);
 
+        log.info("[CategoryServiceImpl] :: [getCategoryById] :: Category with Id [{}] has been updated successfully...", id);
         return modelMapper.map(category,CategoryResponseDTO.class);
     }
 
